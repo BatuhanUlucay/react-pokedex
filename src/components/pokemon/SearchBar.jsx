78e6dfb55c6878fd0f9d6d6a3fb8fb0910eae4d5
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-06-09 09:30:03 +0300

    simple searchbar

--- src/components/pokemon/SearchBar.jsx
+++ src/components/pokemon/SearchBar.jsx
@@ -1,36 +1,46 @@
-import React from 'react'
+import React from "react";
 
 function SearchBar() {
   return (
-    <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
-      <div>
-        <form className="form-control">
-          <div className="relative">
-            <input
-              type="text"
-              className="w-full pr-40 bg-gray-200 input input-lg text-black"
-              placeholder="Search"
-              value="asd"
-              
-            ></input>
-            <button
-              type="submit"
-              className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"
-            >
-              Go
-            </button>
-          </div>
-        </form>
-      </div>
-      {1 > 0 && (
-        <div>
-          <button className="btn btn-ghost btn-lg">
-            Clear
-          </button>
-        </div>
-      )}
+    <div className="h-16 flex items-center max-w-md mx-auto shadow rounded">
+      <input
+        type="text"
+        className="bg-gray-200 input-lg text-black flex-grow"
+        placeholder="Search"
+      ></input>
     </div>
-  )
+  );
+
+  // return (
+  //   <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
+  //     <div>
+  //       <form className="form-control">
+  //         <div className="relative">
+  //           <input
+  //             type="text"
+  //             className="w-full pr-40 bg-gray-200 input input-lg text-black"
+  //             placeholder="Search"
+  //             value="asd"
+
+  //           ></input>
+  //           <button
+  //             type="submit"
+  //             className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"
+  //           >
+  //             Go
+  //           </button>
+  //         </div>
+  //       </form>
+  //     </div>
+  //     {1 > 0 && (
+  //       <div>
+  //         <button className="btn btn-ghost btn-lg">
+  //           Clear
+  //         </button>
+  //       </div>
+  //     )}
+  //   </div>
+  // )
 }
 
-export default SearchBar
+export default SearchBar;
