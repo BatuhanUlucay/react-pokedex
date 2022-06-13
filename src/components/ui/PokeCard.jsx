04f32368ab9a936f2a001928345f7df9cb350a2f
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-06-13 22:29:25 +0300

    rendered the pokemon description

--- src/components/ui/PokeCard.jsx
+++ src/components/ui/PokeCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PokemonType from "../pokemon/PokemonType";
 import Card from "./Card";
+import { Link } from "react-router-dom";
 
 function PokeCard({ id, name, types }) {
   const numberPadding = (id) => {
@@ -14,25 +15,27 @@ function PokeCard({ id, name, types }) {
   };
 
   return (
-    <Card className="min-w-72 bg-base-100 shadow-xl border-2 border-solid">
-      <figure>
-        <img
-          src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${numberPadding(
-            id
-          )}.png`}
-          alt="Pokemon"
-        />
-      </figure>
+    <Link to={`/pokemons/${name}`}>
+      <Card className="min-w-72 bg-base-100 shadow-xl border-2 border-solid">
+        <figure>
+          <img
+            src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${numberPadding(
+              id
+            )}.png`}
+            alt="Pokemon"
+          />
+        </figure>
 
-      <div className="card-body">
-        <h2 className="card-title justify-center">
-          {name.replace(/^./, name[0].toUpperCase())}
-        </h2>
-        <div className="card-actions justify-center">
-          <PokemonType types={types} />
+        <div className="card-body">
+          <h2 className="card-title justify-center">
+            {name.replace(/^./, name[0].toUpperCase())}
+          </h2>
+          <div className="card-actions justify-center">
+            <PokemonType types={types} />
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </Link>
   );
 }
 
