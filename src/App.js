01525fbb1c50e4e9981c0921d9deec497240d649
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-06-15 19:45:36 +0300

    Context provider is moved to the Home components.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import PokemonInfo from "./pages/PokemonInfo";
 import Footer from "./components/layout/Footer";
-import { PokemonProvider } from "./context/PokemonContext";
 
 function App() {
   // You do not have to put the provider here. I think it would be more appropriate if you put it inside Home.js
   // You are exposing the context to the components do not use the component.
   return (
     <>
-      <PokemonProvider>
         <Router>
           <div className="flex flex-col justify-between h-screen">
             <Navbar />
@@ -28,7 +26,6 @@ function App() {
             <Footer />
           </div>
         </Router>
-      </PokemonProvider>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import SearchBar from "../components/ui/SearchBar";
 import PokemonList from "../components/pokemon/PokemonList";
 import RegionFilter from "../components/ui/RegionFilter";
+import { PokemonProvider } from "../context/PokemonContext";
 
 function Home() {
   return (
-    <>
+    <PokemonProvider>
       <div className="h-16 flex items-center max-w-screen-lg mx-auto my-16">
         <SearchBar />
         <RegionFilter/>
       </div>
       <PokemonList />
-    </>
+    </PokemonProvider>
   );
 }
 
