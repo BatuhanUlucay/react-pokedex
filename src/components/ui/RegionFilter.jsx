a660313169c5d399514a48aa7570ea455f1e65c3
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-06-16 23:48:31 +0300

    Filter logic is migrated to context and component got cleaner.

--- src/components/ui/RegionFilter.jsx
+++ src/components/ui/RegionFilter.jsx
@@ -1,95 +1,20 @@
 import React from "react";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import PokemonContext from "../../context/PokemonContext";
 
 function RegionFilter() {
-  const { pokemons, setPokemonsFiltered} = useContext(PokemonContext);
-
-useEffect(() => {
-    setPokemonsFiltered(pokemons);
-}, [pokemons]); // eslint-disable-line
-
+  const { setPokemonFilter, pokemonFilter } = useContext(PokemonContext);
 
   const handleChange = (event) => {
-    // setFilter(event.target.value || "All Pokemons")
-    // I think you should not put the filtering logic inside a component.
-    // It is strictly tied to the context.
-    // That way you can persist the filter state in the dropdown. Now it forgets what you selected if you go to detail, and back to the home page.
-    // Of course it requires a corresponding change in the context as well
-
-    switch (event.target.value) {
-      case "All pokemons":
-        setPokemonsFiltered(pokemons);
-        break;
-      case "Kanto":
-        setPokemonsFiltered(
-          pokemons.filter((pokemon, index) => {
-            return index < 151;
-          })
-        );
-        break;
-      case "Johto":
-        setPokemonsFiltered(
-          pokemons.filter((pokemon, index) => {
-            return index >= 151 && index < 251;
-          })
-        );
-        break;
-      case "Hoenn":
-        setPokemonsFiltered(
-          pokemons.filter((pokemon, index) => {
-            return index >= 251 && index < 386;
-          })
-        );
-        break;
-      case "Sinnoh":
-        setPokemonsFiltered(
-          pokemons.filter((pokemon, index) => {
-            return index >= 386 && index < 494;
-          })
-        );
-        break;
-      case "Unova":
-        setPokemonsFiltered(
-          pokemons.filter((pokemon, index) => {
-            return index >= 494 && index < 649;
-          })
-        );
-        break;
-      case "Kalos":
-        setPokemonsFiltered(
-          pokemons.filter((pokemon, index) => {
-            return index >= 649 && index < 721;
-          })
-        );
-        break;
-      case "Alola":
-        setPokemonsFiltered(
-          pokemons.filter((pokemon, index) => {
-            return index >= 721 && index < 809;
-          })
-        );
-        break;
-      case "Galar":
-        setPokemonsFiltered(
-          pokemons.filter((pokemon, index) => {
-            return index >= 809;
-          })
-        );
-        break;
-      default:
-        setPokemonsFiltered(pokemons);
-        break;
-    }
-  }
-        
+    setPokemonFilter(event.target.value || "All Pokemons");
+  };
 
   return (
     <>
       <select
         className="select select-bordered select-lg w-full max-w-xs ml-16"
         onChange={handleChange}
-        // value={filter} filter should come from the context
+        value={pokemonFilter}
       >
         <option>All Pokemons</option>
         <option>Kanto</option>
