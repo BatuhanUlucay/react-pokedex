1564307f097f5164236333ef81215d020d40a284
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-06-17 00:05:07 +0300

    useQuery is used for cacheing data.

--- src/components/pokemon/PokemonList.jsx
+++ src/components/pokemon/PokemonList.jsx
@@ -1,39 +1,70 @@
-import React from "react";
+import React, { useContext } from "react";
 import PokeCard from "../ui/PokeCard";
-import {  useEffect, useContext } from "react";
-import PokemonContext from "../../context/PokemonContext";
 import LoadingSpinner from "../ui/LoadingSpinner";
+import { useQuery } from "react-query";
+import axios from "axios";
+import PokemonContext from "../../context/PokemonContext";
+
+const POKEAPI_URL = "https://pokeapi.co/api/v2/";
 
 function PokemonList() {
-  const { getAllPokemons, loading, pokemonsSearched } = useContext(PokemonContext);
 
-  useEffect(() => {
-    // You call this method everyime user comes to this page
-    // Semantically it is not wrong, but you might do some kind of caching. for example: if you have pokemons do not call this method
-    // Calling this method every time also causes the flicker at first.
-    // But we have pokemon at hand, we can show them.
-    getAllPokemons(0, 898);
-  }, []); // eslint-disable-line
+  const {filterPokemons, searchedText} = useContext(PokemonContext);
 
+  const {
+    isLoading: pokemonNamesIsLoading,
+    error: pokemonNamesError,
+    data: pokemonNamesData,
+    isFetching: pokemonNamesIsFetchnig,
+    isSuccess: pokemonNamesIsSuccess,
+  } = useQuery("pokemonNames", async () => {
+    return axios
+      .get(`${POKEAPI_URL}pokemon?limit=898&offset=0`)
+      .then((res) => res.data);
+  });
 
-  if (loading) {
+  const pokemonNames = pokemonNamesData?.results;
+
+  console.log(pokemonNames);
+
+  const {
+    data: allPokemons,
+    isSuccess: allPokemonsIsSuccess,
+    isLoading: allPokemonsIsLoading,
+  } = useQuery("allPokemons", () => fetchPokemons(pokemonNames), {
+    enabled: !!pokemonNames,
+  });
+
+  const fetchPokemons = async (pokemonNames) => {
+    return Promise.all(
+      pokemonNames.map(async (pokemonItem) => {
+        return axios
+          .get(`https://pokeapi.co/api/v2/pokemon/${pokemonItem.name}`)
+          .then((result) => result.data);
+      })
+    );
+  };
+
+  if (pokemonNamesIsLoading || allPokemonsIsLoading) {
     return <LoadingSpinner />;
   }
 
-  return (
-    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
-      {pokemonsSearched.map((pokemon) => {
-        return (
-          <PokeCard
-            name={pokemon.name}
-            types={pokemon.types}
-            key={pokemon.id}
-            id={pokemon.id}
-          />
-        );
-      })}
-    </div>
-  );
+  else if (allPokemonsIsSuccess) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+        {allPokemons.filter((pokemon, index) => {return index >= filterPokemons()[0] && index <= filterPokemons()[1] && pokemon.name.includes(searchedText)}).map((pokemon) => {
+          return (
+            <PokeCard
+              name={pokemon.name}
+              types={pokemon.types}
+              key={pokemon.id}
+              id={pokemon.id}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default PokemonList;
