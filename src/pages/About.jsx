aaa0489ca32b5a67a08d36a796f1fb980fe4e9ef
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-06-23 16:49:44 +0300

    about page text is added.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function About() {
   return (
-    <h1>About</h1>
+    <>
+    <h1 className='text-4xl my-8'>About this project</h1>
+    <p>I am pretty new to web technologies. And this is my very first react project. I used PokeAPI for data, Tailwind and DaisyUI for styling.</p>
+    <p>You can support me by starring the repo, any suggestions or pull requests are accepted ofc!</p>
+    </>
   )
 }
 
