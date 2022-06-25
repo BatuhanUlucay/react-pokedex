7560219f267c6e40433f137bac9635f597c65282
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-06-25 11:38:00 +0300

    about page styling.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 function About() {
   return (
-    <div className="h-16 max-w-screen-lg my-16">
+    <div className="h-screen max-w-screen-lg my-32">
 
     <h1 className='text-4xl my-8'>About this project</h1>
-    <p>I am pretty new to web technologies. And this is my very first react project. I used PokeAPI for data, Tailwind and DaisyUI for styling.</p>
+    <p>I am pretty new to web technologies. And this is my very first react project.</p>
+    <p className='my-8'>I used PokeAPI for data, Tailwind and DaisyUI for styling.</p>
     <p>You can support me by starring the repo, any suggestions or pull requests are accepted ofc!</p>
     </div>
   )
